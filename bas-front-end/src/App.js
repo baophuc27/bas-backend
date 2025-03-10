b6dff7e3a20a6d3801185777faffffa82a70821b
phatthanh69 <camnemay@gmail.com>
2025-03-10 13:25:37 +0700

    update schedule logout for inactive user

--- bas-front-end/src/App.js
+++ bas-front-end/src/App.js
@@ -1,5 +1,5 @@
 import { PageLoader } from "common/components";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
@@ -7,12 +7,21 @@ import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "redux/store";
 import { setupAuthAxiosClient } from "setup/axios/auth.axios";
 import { router } from "setup/router";
-// import SwaggerUI from "swagger-ui-react";
-// import "swagger-ui-react/swagger-ui.css";
+import { ActiveService } from "services/active.service";
 
 // Notes: <Provider /> must be the root component to prevent strange redux behaviour
 const App = (props) => {
   setupAuthAxiosClient(store);
+
+  useEffect(() => {
+    // Start checking user active status when app initializes
+    ActiveService.startActiveCheck(store);
+
+    // Clean up interval when app unmounts
+    return () => {
+      ActiveService.stopActiveCheck();
+    };
+  }, []);
 
   return (
     <Provider store={store}>
